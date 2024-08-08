ec7b5f012a903206953a0c04a6d5bae6776ce306
Jonathan Redwine <jredwine1@gmail.com>
2024-08-08 14:41:43 -0400

    Small changes, including updating font family

--- src/entities/about/About.js
+++ src/entities/about/About.js
@@ -14,7 +14,12 @@ class About extends Component {
             <div className="about__main__info-about">
               <div className="about__main__info-about-name about-item">Jonathan H. Redwine</div>
               <div className="about__main__info-about-tagline about-item">
-                A software developer with a passion for building things.
+                A software developer with a passion for building things
+              </div>
+              <div className="about__main__info-about-description">
+                I live in Weymouth, Massachusetts with my wife and son. In my free time I enjoy spending time with my
+                family and friends, golfing, and reading. I'm always open to working on exciting projects, so please
+                reach out if you have something in mind!
               </div>
             </div>
             <div className="about__main__info-contact">
